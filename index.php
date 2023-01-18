e9d93ea7d0fbd0a580570e0fff048a7cdb5ae76c
Fayssal Belghazi <faysstoface@hotmail.fr>
2023-01-18 17:19:15 +0100

    Debut projet marmiton

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marmiton</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <div class="container">
+        <div class="logo-wrapper">
+            <a href="index.php">
+                <img src="img/logo_marmiton_2019.png" alt="Marmiton" class="logo">
+            </a>
+        </div>
+        <h1>Nos recettes</h1>
+        <div class="recipes"></div>
+    </div>
+    
+</body>
+</html>
